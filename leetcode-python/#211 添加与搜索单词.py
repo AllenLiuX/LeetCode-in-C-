96e380abe65451c054b5afbcfe7cd573ffb2d4d2
VincentLiu <13120200491@163.com>
2020-04-10 23:35:20 +0800

    data structure dic implementation

---
+++ leetcode-python/#211 添加与搜索单词.py
@@ -0,0 +1,67 @@
+# 设计一个支持以下两种操作的数据结构：
+#
+# void addWord(word)
+# bool search(word)
+# search(word) 可以搜索文字或正则表达式字符串，字符串只包含字母 . 或 a-z 。 . 可以表示任何一个字母。
+#
+# 示例:
+#
+# addWord("bad")
+# addWord("dad")
+# addWord("mad")
+# search("pad") -> false
+# search("bad") -> true
+# search(".ad") -> true
+# search("b..") -> true
+# 说明:
+#
+# 你可以假设所有单词都是由小写字母 a-z 组成的。
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/add-and-search-word-data-structure-design
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+class WordDictionary:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.dic = {}
+
+    def addWord(self, word: str) -> None:
+        """
+        Adds a word into the data structure.
+        """
+        l = len(word)
+        if l in self.dic:
+            self.dic[l] += [word]
+        else:
+            self.dic[l] = [word]
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+        """
+        l = len(word)
+        if l not in self.dic:
+            return False
+        for i in self.dic[l]:
+            if l != len(i):
+                continue
+            match = True
+            for c in range(l):
+                if word[c] != i[c] and word[c] != '.':
+                    match = False
+                    break
+            if match:
+                return True
+        return False
+
+
+
+# Your WordDictionary object will be instantiated and called as such:
+obj = WordDictionary()
+obj.addWord("abc")
+param_2 = obj.search(".bc")
+print(param_2)
